023f4f88f69934b97e3b3aafec9f2fb203fdd05b
Manroj Gill <manrojgill@hotmail.com>
2026-01-03 01:40:21 -0700

    Hero section Done

---
+++ Frontend/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { assets } from "../assets/assests";
+
+const Hero =() => {
+  return (
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+        {/* Left Section */}
+        <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+            <div className="text-[#414141]">
+                <div className="flex items-center gap-2">
+                    <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+                    <p className="font-medium text-sm md:text-base">Our Bestsellers</p>
+                </div>
+                <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">Latest Arrvials</h1>
+                <div className="flex items-center gap-2">
+                    <p className="font-semibold text-sm md:text-base">Shop now</p>
+                    <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+
+                </div>
+            </div>
+        </div>
+        {/* Right Section */}
+        <img src={assets.hero_img} className="w-full sm:w-1/2 aspect-[5/4] object-cover" alt="" />
+    </div>
+  )
+}
+
+export default Hero
