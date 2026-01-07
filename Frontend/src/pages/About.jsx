1c947c71c799a2851e97a71c3d0147bb59680220
Manroj Gill <manrojgill@hotmail.com>
2026-01-07 02:46:43 -0700

    Finish contact and about pages

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -5,17 +5,38 @@ import { assets } from "../assets/assests";
 const About = () => {
   return (
     <div className="">
-      <div className="text-x text-centr pt-8 border-t">
-        <Title text1={"ABOUT"} text2={"US"}/>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"THIS PROJECT"}/>
       </div>
       <div className="my-10 flex flex-col md:flex-row gap-16">
-        <img src={assets.about_img} className="md:max-w-[450px] w-full min-w-[1100px] aspect-[338/117] object-cover" alt="" />
+        <img src={assets.about_img} className="md:max-w-[450px] w-full aspect-[1029/1046] object-cover" alt="" />
         <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
-          <p></p>
-          <p></p>
+          <p>VoltCart is a full-stack e-commerce project designed to streamline the way users explore and purchase electronic components. Built with a focus on performance, modular design, and maintainability, the platform emphasizes clear product organization, responsive UI, and a scalable foundation for future backend expansion.</p>
+          <p>Since starting the project, I’ve focused on designing a clean, well-structured interface that reflects how engineers and students actually think about components. From organizing products by category and specifications to building a responsive UI, each part of VoltCart is shaped by real-world use rather than generic storefront patterns.</p>
+          <b className="text-gray-800">My Mission</b>
+          <p>My goal with VoltCart is to combine practical electronics knowledge with modern web development practices. The project serves as both a learning platform and a foundation for future full-stack expansion, emphasizing clarity, performance, and scalability.</p>
         </div>
-
       </div>
+        <div className="text-xl py-4">
+          <Title text1={"WHY"} text2={"HIRE ME"}/>
+        </div>
+         <div className="flex flex-col md:flex-row text-sm mb-20">
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Engineering Mindset</b>
+            <p className="text-gray-600"> I approach problems with an engineering-first mindset, focusing on clear requirements, structured solutions, and attention to detail. This allows me to build features that are practical, reliable, and easy to maintain.</p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Practical Software Skills</b>
+            <p className="text-gray-600">Through projects like VoltCart, I apply modern frontend tools while designing with scalability and backend integration in mind. Building structured, well-organized systems strengthens my core problem-solving skills and prepares me to adapt quickly to new programming languages and technologies.</p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Strong Ownership & Initiative</b>
+            <p className="text-gray-600">I take full ownership of my work from planning and implementation to iteration and refinement. I’m proactive about learning new technologies, improving designs, and delivering polished results.</p>
+          </div>
+
+          
+
+        </div>
   
     </div>
   )
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assests";
+import resumePdf from "../assets/resume.pdf"
 
 const Contact =() => {
   return (
     <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"ME"}/>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img src={assets.contact_img} className="w-full md:max-w-[490px] md:max-h-[490px]   aspect-1/1" alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Contact Me</p>
+          <p className="text-gray-500">Edmonton, AB, Canada</p>
+          <p className="text-gray-500">Email:manrojgill@hotmail.com <br/> Phone:(780) 257-9105</p>
+          <p className="text-gray-500">LinkedIn: linkedin.com/in/manrojgill <br/>Github: github.com/M-GII</p>
+          <p className="font-semibold text-xl text-gray-600">Opportunities</p>
+          <p className="text-gray-500">Open to co-op and internship roles in software, full-stack, and computer engineering.</p>
+          <button  onClick={() => window.open(resumePdf, "_blank", "noopener,noreferrer")} className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">View Resume</button>
+        </div>
+
+      </div>
   
     </div>
   )
